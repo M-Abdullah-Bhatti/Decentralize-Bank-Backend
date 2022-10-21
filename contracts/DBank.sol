e345be1f4f8a9d3c7a99254ee6c0272c6b979e47
Shayan Jamil <76084810+ShayanJamil519@users.noreply.github.com>
2022-10-21 10:47:03 +0500

    Changes requires to revert and some small changes

--- contracts/DBank.sol
+++ contracts/DBank.sol
@@ -4,10 +4,15 @@ pragma solidity ^0.8.10;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-// import "./BankToken.sol";
+error Bank__MsgSender_NotMinter();
+error Bank__LessDeposit(string);
+error Bank__NoPreviousDeposit();
+error Bank__OverWithDraw(string);
+error Bank__LessCollateral(string);
+error Bank__LoanAlreadyActive();
+error Bank__NoActiveLoan();
 
 contract Bank is ERC20 {
-
     // BankToken private token;
     address public minter;
 
@@ -22,39 +27,49 @@ contract Bank is ERC20 {
     event MinterChanged(address indexed from, address to);
 
     event Deposit(address indexed user, uint256 etherAmount, uint256 timeStart);
-    event Withdraw(address indexed user,uint256 etherAmount, uint256 depositTime,uint256 interest);
-    event Borrow(address indexed user,uint256 collateralEtherAmount,uint256 borrowedTokenAmount);
+    event Withdraw(
+        address indexed user,
+        uint256 etherAmount,
+        uint256 depositTime,
+        uint256 interest
+    );
+    event Borrow(
+        address indexed user,
+        uint256 collateralEtherAmount,
+        uint256 borrowedTokenAmount
+    );
     event Payoff(address indexed user, uint256 fee);
 
-    constructor() ERC20("Bank Token", "BTK"){
-        minter=msg.sender;
+    constructor() ERC20("Bank Token", "BTK") {
+        minter = msg.sender;
     }
 
-    modifier onlyAdmin{
-        require(msg.sender == minter, "Only owner can change the Minter");
+    modifier onlyAdmin() {
+        if (msg.sender != minter) {
+            revert Bank__MsgSender_NotMinter();
+        }
         _;
     }
 
-
-    function mint(address account, uint256 amount) public {
-        require(msg.sender == minter, "Error : msg.sender has to be the minter");
+    function mint(address account, uint256 amount) public onlyAdmin {
         _mint(account, amount);
     }
 
-     function grantRole(address dbank) public virtual  onlyAdmin returns(bool){
-        minter=dbank;
+    function grantRole(address dbank) public onlyAdmin returns (bool) {
+        minter = dbank;
         emit MinterChanged(msg.sender, dbank);
         return true;
     }
 
-
     function deposit() public payable {
-
         //check if msg.value is >= than 0.01 ETH
-        require(
-            msg.value >= 1e16,
-            "Error: Deposit must be greater than 0.01 ETH"
-        );
+        // require(
+        //     msg.value >= 1e16,
+        //     "Error: Deposit must be greater than 0.01 ETH"
+        // );
+        if (msg.value <= 1e16) {
+            revert Bank__LessDeposit("Deposit must be greater than 0.01 ETH");
+        }
 
         //increase msg.sender ether deposit balance
         etherBalanceOf[msg.sender] = etherBalanceOf[msg.sender] + msg.value;
@@ -63,15 +78,23 @@ contract Bank is ERC20 {
         //set msg.sender deposit status to true
         isDeposited[msg.sender] = true;
         //emit Deposit event
-        emit Deposit(msg.sender, _amount, block.timestamp);
+        emit Deposit(msg.sender, msg.value, block.timestamp);
     }
 
-    function withdraw(uint256 amount) public {
+    function withdraw(uint256 amount) public payable{
         //check if msg.sender deposit status is true
-        require(isDeposited[msg.sender] == true, "Error: No previous deposit");
-    
+        // require(isDeposited[msg.sender] == true, "Error: No previous deposit");
+        if (isDeposited[msg.sender] == false) {
+            revert Bank__NoPreviousDeposit();
+        }
+
         //assign msg.sender ether deposit balance to variable for event
         uint256 userBalance = etherBalanceOf[msg.sender];
+
+        // require(amount <= userBalance, "Error: Withdraw can't be more than deposit");
+        if (amount >= userBalance) {
+            revert Bank__OverWithDraw("Withdraw can't be more than deposit");
+        }
         //check user's hodl time
         uint256 depositTime = block.timestamp - depositStart[msg.sender];
         //calc interest per second with 10% APY
@@ -85,25 +108,34 @@ contract Bank is ERC20 {
         //send interest in tokens to user
         mint(msg.sender, interest);
         //reset depositer data
+        
+        // isDeposited[msg.sender] = false;
         depositStart[msg.sender] = 0;
         etherBalanceOf[msg.sender] = 0;
-        isDeposited[msg.sender] = false;
         emit Withdraw(msg.sender, userBalance, depositTime, interest);
-        
-
     }
 
     function borrow() public payable {
-        //check if collateral is >= than 0.01 ETH
-        require(
-            msg.value >= 1e16,
-            "Error: Collateral amount must be greater than 0.01 ETH"
-        );
         //check if user doesn't have active loan
-        require(
-            isBorrowed[msg.sender] == false,
-            "Error: Loan is already active"
-        );
+        // require(
+        //     isBorrowed[msg.sender] == false,
+        //     "Error: Loan is already active"
+        // );
+        if (isBorrowed[msg.sender] == true) {
+            revert Bank__LoanAlreadyActive();
+        }
+
+        //check if collateral is >= than 0.01 ETH
+        // require(
+        //     msg.value >= 1e16,
+        //     "Error: Collateral amount must be greater than 0.01 ETH"
+        // );
+        if (msg.value <= 1e16) {
+            revert Bank__LessCollateral(
+                "Collateral amount must be greater than 0.01 ETH"
+            );
+        }
+
         //add msg.value to ether collateral
         collateralEther[msg.sender] = collateralEther[msg.sender] + msg.value;
         //calc tokens amount to mint, 50% of msg.value
@@ -116,15 +148,21 @@ contract Bank is ERC20 {
         emit Borrow(msg.sender, collateralEther[msg.sender], tokensToMint);
     }
 
-    function payOff() public  {
+    function payOff() public {
         //check if loan is active
-        require(
-            isBorrowed[msg.sender] = true,
-            "Error: User Doesn;t have any active loan"
-        );
+        // require(
+        //     isBorrowed[msg.sender] == true,
+        //     "Error: User Doesn;t have any active loan"
+        // );
+        if(isBorrowed[msg.sender] ==false ){
+            revert Bank__NoActiveLoan();
+        }
 
- // first you have to approve the msg.sender to tranfer the tokens
-    require(approve(msg.sender, collateralEther[msg.sender] / 2), "This address is not approved for transferring token");
+        // first you have to approve the msg.sender to tranfer the tokens
+        require(
+            approve(msg.sender, collateralEther[msg.sender] / 2),
+            "This address is not approved for transferring token"
+        );
 
         //transfer tokens from user back to the contract
         require(
@@ -146,30 +184,25 @@ contract Bank is ERC20 {
         emit Payoff(msg.sender, fee);
     }
 
-
-
     // Getter Functions
-    
 
-    function getEtherBalanceOf() external view returns(uint256){
+    function getEtherBalanceOf() external view returns (uint256) {
         return etherBalanceOf[msg.sender];
     }
 
-    function getDepositStartTime(address user) external view returns(uint256){
+    function getDepositStartTime(address user) external view returns (uint256) {
         return depositStart[user];
     }
 
-    function getCollateralEther(address user) external view returns(uint256){
+    function getCollateralEther(address user) external view returns (uint256) {
         return collateralEther[user];
     }
 
-    function chechIsDeposited(address user) external view returns(bool){
+    function chechIsDeposited(address user) external view returns (bool) {
         return isDeposited[user];
     }
 
-    function chechIsBorrowed(address user) external view returns(bool){
+    function chechIsBorrowed(address user) external view returns (bool) {
         return isBorrowed[user];
     }
-
 }
-
